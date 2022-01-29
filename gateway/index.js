96fc0f3409b19f4d6fdeab6908137b077144b90f
FODIL Zine-eddine <zineeddine.fodil99@gmail.com>
2022-01-29 20:16:44 +0100

    fixing the dockerization of the front w cors errors

--- gateway/index.js
+++ gateway/index.js
@@ -5,15 +5,16 @@ const proxy = require("express-http-proxy");
 const app = express();
 
 
-
-app.use(cors());
-app.use(express.json());
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
     next();
 });
+
+app.use(cors());
+app.use(express.json());
+
+
 
 
 app.use('/auth',proxy("http://auth:8001"));
