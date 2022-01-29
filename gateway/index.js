047a1180fa96fe310d59025f10e3e2c7a40262b1
FODIL Zine-eddine <zineeddine.fodil99@gmail.com>
2022-01-29 17:03:10 +0100

    changes

--- gateway/index.js
+++ gateway/index.js
@@ -4,9 +4,16 @@ const proxy = require("express-http-proxy");
 
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://gateway:8000");
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
+    next();
+});
 
 
 app.use('/auth',proxy("http://auth:8001"));
